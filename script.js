38c48335bba66131a0e863b100a6575de61722ac
Tiffany <sunngreen@gmail.com>
2016-09-07 21:43:47 -0700

    add elements to dom

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-var request = new XMLHttpRequest(); 
-request.open('GET', 'data.txt', false); 
-request.send(); 
-console.log(request); 
-console.log('hello'); 
+for(var i = 0; i < 600; i++){	
+	var request = new XMLHttpRequest(); 
+	request.open('GET', 'data.txt'); 
+	request.onreadystatechange = function(){
+		if (request.readyState===4){
+			console.log(request); 
+			document.writeln(request.responseText); 
+		}
+	}
+	request.send(); 
+}
