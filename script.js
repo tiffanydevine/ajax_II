d859158f4504525c3df74b4f91be5807a5000d3c
Tiffany <sunngreen@gmail.com>
2016-09-07 23:25:47 -0700

    add load button for page using event listener

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var request = new XMLHttpRequest(); 
-request.open('GET', 'data.json'); 
-request.onreadystatechange = function(){
-	if (request.readyState===4){
-		var items = JSON.parse(request.responseText); 
-		console.log(items)
-		var output = '<ul>'
-		for (var i=0; i < items.length; i++){
-			output += '<li>' + items[i].first_name + ' ' + items[i].last_name + '</li>'
+function loadAJAX() {
+	var request = new XMLHttpRequest(); 
+	request.open('GET', 'data.json'); 
+	request.onreadystatechange = function(){
+		if (request.readyState===4){
+			var items = JSON.parse(request.responseText); 
+			console.log(items)
+			var output = '<ul>'
+			for (var i=0; i < items.length; i++){
+				output += '<li>' + items[i].first_name + ' ' + items[i].last_name + '</li>'
+			}
+			output += '</ul>'
+			document.getElementById('update').innerHTML = output; 
 		}
-		output += '</ul>'
-		document.getElementById('update').innerHTML = output; 
 	}
+	request.send(); 
 }
-request.send(); 
 
 
