41221353a917a8db50bd58aeeb2199822aa57604
Tiffany <sunngreen@gmail.com>
2016-09-07 22:01:09 -0700

    update dom with getElementByTagName

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ for(var i = 0; i < 600; i++){
 	request.open('GET', 'data.txt'); 
 	request.onreadystatechange = function(){
 		if (request.readyState===4){
-			var modify = document.getElementById('update'); 
-			modify.innerHTML = request.responseText;
+			var modify = document.getElementsByTagName('li'); 
+			modify[3].innerHTML = request.responseText;
 		}
 	}
 	request.send(); 
