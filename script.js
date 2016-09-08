41edcfed6f94b4d719f2e29b369bf17af8c9742e
Tiffany <sunngreen@gmail.com>
2016-09-07 23:18:21 -0700

    add json to list

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-for(var i = 0; i < 600; i++){	
-	var request = new XMLHttpRequest(); 
-	request.open('GET', 'data.txt'); 
-	request.onreadystatechange = function(){
-		if (request.readyState===4){
-			console.log(request); 
-			document.writeln(request.responseText); 
+var request = new XMLHttpRequest(); 
+request.open('GET', 'data.json'); 
+request.onreadystatechange = function(){
+	if (request.readyState===4){
+		var items = JSON.parse(request.responseText); 
+		console.log(items)
+		var output = '<ul>'
+		for (var i=0; i < items.length; i++){
+			output += '<li>' + items[i].first_name + ' ' + items[i].last_name + '</li>'
 		}
+		output += '</ul>'
+		document.getElementById('update').innerHTML = output; 
 	}
-	request.send(); 
 }
+request.send(); 
+
 
