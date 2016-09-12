8e2563e1b1e177e3988b1aba4f8cb00beebb13a3
Tiffany <sunngreen@gmail.com>
2016-09-12 09:53:13 -0700

    access NBA API with $.getJSON

--- jsonp_test.js
+++ jsonp_test.js
@@ -1,3 +1,4 @@
- $.getJSON('http://stats.nba.com/stats/commonplayerinfo?LeagueID=00&PlayerID=202329&SeasonType=Regular+Season', function (data) {
-    console.log(data);
+var thing = $.getJSON('http://stats.nba.com/stats/commonplayerinfo?LeagueID=00&PlayerID=202329&SeasonType=Regular+Season', function (data) {
+  	console.log(data.resultSets[0].rowSet[0][3]); 
   });
+
