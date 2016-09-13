346bf2a7b241b78a00e5291cf52aa45554bb4c1d
Tiffany <sunngreen@gmail.com>
2016-09-13 10:01:58 -0700

    load players into dom

--- blz.js
+++ blz.js
@@ -1,9 +1,13 @@
 
 $.getJSON('http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&GameSegment=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PaceAdjust=N&PerMode=PerGame&Period=0&PlusMinus=N&Rank=N&Season=2015-16&SeasonSegment=&SeasonType=Regular+Season&TeamID=1610612757&VsConference=&VsDivision=', function (data) {
-  	var output = '<ul>' 
+  	var output = "<ul class='searchresults'>"; 
+
+
   	$.each(data.resultSets[1].rowSet, function(index, value){
   		console.log(value[2]); 
-  		output += '<li>' + value[2] + '</li>'; 
+  		output += '<li>';  
+  		output += '<h2>' + value[2] + '</h2>';
+  		output += '</li>' 
   	}); 
   	output += '</ul>'; 
   	$('#update').append(output); 
