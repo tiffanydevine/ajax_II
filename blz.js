b456eea46aebe03395d28c80ecb30b0e762896f0
Tiffany <sunngreen@gmail.com>
2016-09-14 09:51:14 -0700

    rename fields

--- blz.js
+++ blz.js
@@ -7,7 +7,7 @@ $('#search').keyup(function () {
 		  	if (value[2].search(myExp) != -1){	
 		  		output += '<li>';  
 		  		output += '<h2>' + value[2] + '</h2>';
-		  		output += '<img src="images/' + value[2] + '.png" alt="not found" />'; 
+		  		output += '<img src="images/' + value[2] + '.png" id="blzpics" alt="not found" />'; 
 		  		output += '<p>Field goal percentage: ' + (value[10] * 100).toFixed(2)  + ', Three point percentage: ' + (value[13] * 100).toFixed(2)  + ', Rebounds: ' + value[19].toFixed(2) + ', Assists: ' + value[20].toFixed(2) + '</p>'; 
 		  		output += '</li>';  
 		  	}
