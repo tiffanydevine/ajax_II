0dc919a3e75cda7c71ce5d776d1d0dd0df471b6c
Tiffany <sunngreen@gmail.com>
2016-09-13 21:10:33 -0700

    player pics and bios loaded

--- blz.js
+++ blz.js
@@ -5,11 +5,15 @@ $.getJSON('http://stats.nba.com/stats/teamplayerdashboard?DateFrom=&DateTo=&Game
   		console.log(value[2]); 
   		output += '<li>';  
   		output += '<h2>' + value[2] + '</h2>';
-  		output +=  '<img src="images/' + value[2] + '.png" alt="quarterman.png" />'
+  		output += '<img src="images/' + value[2] + '.png" alt="not found" />'
+  		output += '<p>Field goal percentage: ' + (value[10] * 100).toFixed(2)  + ', Three point percentage: ' + (value[13] * 100).toFixed(2)  + ', Rebounds: ' + value[19].toFixed(2) + ', Assists: ' + value[20].toFixed(2) + '</p>'
   		output += '</li>' 
+
+
+
   	}); 
   	output += '</ul>'; 
   	$('#update').append(output); 
-  	console.log(data.resultSets[1].rowSet); 
+  	console.log(data.resultSets); 
   });
 
