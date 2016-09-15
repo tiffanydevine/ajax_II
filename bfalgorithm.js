790ee5819c895c9df6ddfaf2dfb6f685ea9d12c2
Tiffany <sunngreen@gmail.com>
2016-09-15 09:19:05 -0700

    establish label

---
+++ bfalgorithm.js
@@ -0,0 +1,7 @@
+var generosity = $('#generosity').val(); 
+var honesty = $('#honesty').val(); 
+var ambition = $('#ambition').val(); 
+var generosity = $('#generosity').val(); 
+var reliability = $('#reliability').val(); 
+var trustworthy = $('#trustworthy').val(); 
+var badBoy = $('#generosbadBoyity').val(); 
