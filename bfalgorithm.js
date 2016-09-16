90e5c6814fa1fe4f372e4373962735b29c03751c
Tiffany <sunngreen@gmail.com>
2016-09-15 21:10:28 -0700

    select and log slider values

---
+++ bfalgorithm.js
@@ -0,0 +1,25 @@
+// grab slider values 
+
+var generosity = $('#generosity').change(function(){
+    var val = this.value;
+});
+
+var honesty = $('#honesty').change(function(){
+    var val = this.value;
+});
+
+var ambition = $('#ambition').change(function(){
+    var val = this.value;
+});
+
+var generosity = $('#generosity').change(function(){
+    var val = this.value;
+});
+
+var reliability = $('#reliability').change(function(){
+    var val = this.value;
+});
+
+var generosity = $('#generosity').change(function(){
+    var val = this.value;
+});
